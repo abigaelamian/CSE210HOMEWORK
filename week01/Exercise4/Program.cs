9b240a460fae3f385af6e5f669971091adee6e4a
abigaelamian <abbymalina94@gmail.com>
2026-01-23 17:02:09 +0300

    update change in week 1

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,65 @@
 using System;
 
+using System.Collections.Generic;
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int input = -1;
+         while (input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+         int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+         double average = (double)sum / numbers.Count;
+         int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+         Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+ int smallestPositive = int.MaxValue;
+        bool foundPositive = false;
+
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < smallestPositive)
+            {
+                smallestPositive = number;
+                foundPositive = true;
+            }
+        }
+
+        if (foundPositive)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        numbers.Sort();
+
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,45 @@
-using System;
+ using System;
 
-class Program
+namespace Exercise5
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        static void DisplayResult(string name, int squaredNumber)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+        }
+
+        static void Main(string[] args)
+        {
+            DisplayWelcome();
+            string userName = PromptUserName();
+            int favoriteNumber = PromptUserNumber();
+            int squaredNumber = SquareNumber(favoriteNumber);
+            DisplayResult(userName, squaredNumber);
+        }
     }
 }
